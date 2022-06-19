cb8f132c3463efa765e7d846c6f0a637dced8d8c
JakubJukl <jukljakub@gmail.com>
2022-06-19 17:32:30 +0200

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://logs.lubos.workers.dev/",
+  resetDay: 3,
+  resetHour: 6,
+  iconUrl: "http://mop-static.tauri.hu/images/icons/"
 };
