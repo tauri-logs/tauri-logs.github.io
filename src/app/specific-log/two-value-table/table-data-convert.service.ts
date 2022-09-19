07236250749cfa24da2d0f94f6a9664300244877
Jakub Jukl <jukljakub@gmail.com>
2022-09-19 16:09:02 +0200

    number formatting

--- src/app/specific-log/two-value-table/table-data-convert.service.ts
+++ src/app/specific-log/two-value-table/table-data-convert.service.ts
@@ -24,11 +24,11 @@ export class TableDataConvertService {
         //@ts-ignore
         `${raidDetail.encounter_data.encounter_name} ${SOO_DIFFICULTIES[raidDetail.difficulty]}`
       ),
-      new TablePair('Wipes', raidDetail.wipes.toString()),
+      new TablePair('Wipes', this.formatNumber(raidDetail.wipes, 2)),
       new TablePair('Duration',
-        `${Math.floor(raidDetail.fight_time / 60000)}:${Math.floor((raidDetail.fight_time % 60000) / 1000)}`
+        `${this.formatNumber(Math.floor(raidDetail.fight_time / 60000), 2)}:${this.formatNumber(Math.floor((raidDetail.fight_time % 60000) / 1000), 2)}`
       ),
-      new TablePair('Ressurects/Deaths', `${raidDetail.resurrects_fight}/${raidDetail.deaths_fight}`),
+      new TablePair('Ressurects/Deaths', `${this.formatNumber(raidDetail.resurrects_fight, 2)}/${this.formatNumber(raidDetail.deaths_fight, 2)}`),
     ];
   }
 
@@ -39,10 +39,14 @@ export class TableDataConvertService {
   private getComposition(members: Member[]): TablePair[] {
     const composition = new Composition(members);
     return [
-      new TablePair('Tank', composition.tanks.toString()),
-      new TablePair('Healer', composition.healers.toString()),
-      new TablePair('DPS', (composition.mdps + composition.rdps).toString()),
-      new TablePair('Total', composition.total.toString())
+      new TablePair('Tank', this.formatNumber(composition.tanks, 2)),
+      new TablePair('Healer', this.formatNumber(composition.healers, 2)),
+      new TablePair('DPS', this.formatNumber(composition.mdps + composition.rdps, 2)),
+      new TablePair('Total', this.formatNumber(composition.total, 2))
     ]
+  }
+
+  private formatNumber(number: number, digits: number): string {
+    return number.toLocaleString('en-US', {minimumSignificantDigits: digits});
   }
 }
