7d187587761f5f673c05322aba493862539afd26
Jakub Jukl <jukljakub@gmail.com>
2024-02-25 11:03:06 +0100

    logo link

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -8,10 +8,19 @@ import {Component, Input, OnInit} from '@angular/core';
 export class LogoComponent implements OnInit {
 
   @Input() fontSize: string = '8rem';
+  /**
+   * If true, the logo will be a link to the home page
+   */
+  @Input() link: boolean = false;
 
-  constructor() { }
+  cursorStyle!: string;
+  routerLink!: string | null;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.cursorStyle = this.link ? 'pointer' : 'default';
+    this.routerLink = this.link ? '/' : null;
   }
 
 }
